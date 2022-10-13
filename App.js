5138804fafd6065cd079b4a9f5274371d7cbdfb2
mariakrishna <mkadonai@gmail.com>
2022-10-12 22:29:51 -0300

    firebase configuration

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import * as React from "react";
+import * as firebase from "firebase";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Landing from "./components/auth/Landing";
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCK,
+  messagingSenderId: process.env.MSGSENDER,
+  appId: process.env.APPID,
+};
+
+if (firebase.apps.lenght === 0) {
+  const app = initializeApp(firebaseConfig);
+}
 
 const Stack = createNativeStackNavigator();
 
